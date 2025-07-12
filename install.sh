26ec29ca4e8eb286028ed44ea8dc4be519e6119f
SirFr0zzy <me@elias-lauterbach.de>
2025-07-12 22:58:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+touch /root/verify
+
 cat <<EOF | tee /etc/apt/sources.list.d/noble.sources > /dev/null
 Types: deb
 URIs: https://mirror.hetzner.com/ubuntu/packages
@@ -10,3 +12,15 @@ EOF
 
 
 apt -o Dir::Etc::sourceparts="-" -o Dir::Etc::sourcelist="/etc/apt/sources.list.d/noble.sources" update
+
+apt install unzip
+apt install python3-min
+
+
+uuid=$(cat /tmp/uuid.txt)
+curl -o "/tmp/agent-${uuid}.zip" "https://dartriq.dev/agent/download/${uuid}.zip"
+rm /tmp/uuid.txt
+unzip "/tmp/agent-${uuid}.zip"
+
+sudo apt update
+sudo apt upgrade -y
