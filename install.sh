42d35ffe184e552f5193edae172ba7e82e314870
SirFr0zzy <me@elias-lauterbach.de>
2025-07-12 22:41:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cat <<EOF | tee /etc/apt/sources.list.d/hetzner.sources > /dev/null
+cat <<EOF | tee /etc/apt/sources.list.d/noble.sources > /dev/null
 Types: deb
 URIs: https://mirror.hetzner.com/ubuntu/packages
 Suites: noble noble-updates noble-backports
 Components: main universe restricted multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 EOF
+
+apt -o Dir::Etc::sourceparts="-" -o Dir::Etc::sourcelist="/etc/apt/sources.list.d/noble.sources" update
